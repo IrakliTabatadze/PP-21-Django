ba0779aa9f122255803cfac74b8f81b970400394
IrakliTabatadze <irakli.tabatadz.443@gmail.com>
2024-11-04 22:12:08 +0400

    Lecture 38

--- TestProject/testapp/models.py
+++ TestProject/testapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=15, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = 'products'
+
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=20)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name + ' - ' + self.product.name
--- TestProject/testapp/views.py
+++ TestProject/testapp/views.py
@@ -1,8 +1,59 @@
 from django.shortcuts import render, HttpResponse
+from django.db import connection
+from .models import Product, Supplier
+from django.db.models import Q
 
 global_var = "hello world"
 
 def index(request):
+    # cursor = connection.cursor()
+    # cursor.execute('select * from products')
+    # print(cursor.fetchall())
+
+    # product = Product.objects.create(name='Asus Vivobook', description='Asus new laptop', price=1555.55)
+
+    # product = Product()
+    # product.name = 'HP G255'
+    # product.price = 1444.44
+    # product.save()
+
+    # products = Product.objects.all()
+    # print(products)
+
+    # products = Product.objects.filter(name__icontains='pro').first()
+    # print(products)
+
+    # products = Product.objects.filter(name__in=['Macbook Pro', 'HP G255']).all()
+    # print(products)
+
+    # product = Product.objects.get(pk=1)
+    # print(product)
+
+    # products = Product.objects.filter(Q(name__icontains='pro') | Q(name__icontains='hp'))
+    # print(products)
+
+    # products = Product.objects.filter(~Q(name__icontains='hp') & Q(price__gte=1000))
+    # print(products)
+
+    # product = Product.objects.get(pk=1)
+    # product.price = 2500
+    # product.save()
+    # print(product)
+
+    # product = Product.objects.get(pk=4)
+    # product.delete()
+
+
+    # suppliers = Supplier.objects.all()
+    # for supplier in suppliers:
+    #     print(supplier.name, supplier.product.name)
+
+    # suppliers = Supplier.objects.filter(product__name__icontains='G255').first()
+    # print(suppliers)
+
+    supplier = Supplier.objects.select_related('product').all()
+    print(supplier)
+
     return HttpResponse('<h1>Hello From Views.py</h1>')
 
 def test(request):
