8abe4f77fba4074e655253f956d9de3f012dd172
IrakliTabatadze <irakli.tabatadz.443@gmail.com>
2024-11-18 22:16:32 +0400

    Lecture 42

--- TestProject/shop/views.py
+++ TestProject/shop/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render, redirect
 from .models import Category, Product
 from .forms import ProductForm
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     product_name = request.GET.get('product_name')
     category = request.GET.getlist('category')
+
+    print("Categories: ", category)
 
     if product_name:
         products = Product.objects.filter(name__icontains=product_name)
@@ -24,7 +27,6 @@ def index(request):
     except EmptyPage:
         products = paginator.page(paginator.num_pages)
 
-
     categories = Category.objects.all()
     category_by_type = {}
 
@@ -36,10 +38,9 @@ def index(request):
 
     print(category_by_type)
 
-
     return render(request, 'index.html', {'products': products, 'categories_by_type': category_by_type})
 
-
+@login_required(login_url='/users/login/')
 def detail_product(request, pk):
     product = Product.objects.get(pk=pk)
 
@@ -47,14 +48,18 @@ def detail_product(request, pk):
 
     return render(request, 'detail_product.html', {'product': product, 'related_products': related_products})
 
+
 def add_product(request):
-    if request.method == 'POST':
-        form = ProductForm(request.POST)
-        if form.is_valid():
-            product = form.save(commit=False)
-            product.save()
-            form.save_m2m()
-            return redirect('shop:index')
+    if request.user.has_perm('shop.add_product'):
+        if request.method == 'POST':
+            form = ProductForm(request.POST)
+            if form.is_valid():
+                product = form.save(commit=False)
+                product.save()
+                form.save_m2m()
+                return redirect('shop:index')
+        else:
+            form = ProductForm()
+            return render(request, 'add_product.html', {'form': form})
     else:
-        form = ProductForm()
-        return render(request, 'add_product.html', {'form': form})
+        return redirect('shop:index')
--- TestProject/users/views.py
+++ TestProject/users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
+from django.contrib.auth import logout, login
 
-# Create your views here.
+def register_user(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("shop:index")
+        else:
+            return render(request, 'registration/registration.html', {'form': form})
+
+    else:
+        form = UserRegistrationForm()
+
+        return render(request, 'registration/registration.html', {'form': form})
+
+
+def logout_user(request):
+    logout(request)
+
+    return redirect('shop:index')
