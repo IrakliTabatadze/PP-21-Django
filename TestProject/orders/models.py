dcbb6d43243c5b10c00685c3f40ec8e755f4bfbe
IrakliTabatadze <irakli.tabatadz.443@gmail.com>
2024-11-26 00:38:27 +0400

    Lecture 43

--- TestProject/orders/models.py
+++ TestProject/orders/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from shop.models import Product
+from decimal import Decimal
 
-# Create your models here.
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    @property
+    def total(self):
+        return Decimal(self.quantity * self.product.price if self.product else Decimal('0.00'))
--- TestProject/orders/views.py
+++ TestProject/orders/views.py
@@ -1,3 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Cart, CartItem
+from shop.models import Product
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required(login_url='/users/login')
+def cart_view(request):
+    cart, created = Cart.objects.get_or_create(user=request.user)
+
+    cart_items = CartItem.objects.filter(cart=cart)
+
+    total_amount = sum(cart_item.total for cart_item in cart_items)
+
+
+    return render(request, 'cart.html', {'cart_items': cart_items, 'total_amount': total_amount})
+
+
+@login_required(login_url='/users/login')
+def add_cart_item(request, pk):
+
+    product = Product.objects.get(pk=pk)
+
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    print(f'Product Stock: {product.stock}')
+    if product.stock > 0:
+        cart_item, cart_item_created = CartItem.objects.get_or_create(cart=cart, product=product)
+        print(f'Cart Item Created: {cart_item_created}')
+        if not cart_item_created:
+            print(f'CartItem Not Created')
+            cart_item.quantity += 1
+        elif cart_item_created:
+            print(f'CartItem Created')
+            cart_item.quantity = 1
+        cart_item.save()
+
+    return redirect(request.META.get('HTTP_REFERER'))
+
+@login_required(login_url='/users/login')
+def update_cart_item(request, pk):
+    if request.method == 'POST':
+        cart_item = CartItem.objects.get(pk=pk)
+
+        new_quantity = int(request.POST.get('quantity'))
+
+        if new_quantity == 0:
+            cart_item.delete()
+
+        elif new_quantity <= cart_item.product.stock:
+            cart_item.quantity = new_quantity
+            cart_item.save()
+
+    return redirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required(login_url='/users/login')
+def delete_cart_item(request, pk):
+    cart_item = CartItem.objects.get(pk=pk)
+
+    cart_item.delete()
+
+    return redirect(request.META.get('HTTP_REFERER'))
